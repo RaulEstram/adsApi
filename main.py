193e2bee288c24b73da38d4d3e50bc6d838aaa48
RaulEstram <ralesrtada2000@outloook.com>
2022-08-27 22:21:27 -0500

    6

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import json
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return json.dumps({'name': 'alice',
-                       'email': 'alice@outlook.com'})
+    token = "TnEWAPDi8n5R3taijqXleJDTZ5LNDr2LMJjOOsec"
+    results = requests.get(
+        "https://api.adsabs.harvard.edu/v1/search/query?q=docs(06491901cc391f1930e6b5e79207efdb)&rows=50&fl=id, first_author,date,doi,bibcode,author,title&sort=date desc",
+        headers={'Authorization': 'Bearer ' + token})
+
+    data = results.json()
+    return json.dumps(data)
 
 if __name__ == "__main__":
     app.run()
